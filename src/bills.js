968b8bdd61c82975876c4677be26aa2a33892308
Dillon Christensen <j.dillon.christensen@gmail.com>
2021-09-08 21:58:01 -0500

    Q3.2021.updates (#4)
    * updated pricing and removed NE coffee
    
    * fixed up css a bit

--- src/bills.js
+++ src/bills.js
@@ -8,21 +8,21 @@ const bills = [
     price: 113.33,
     autoPay: 1,
     paycheck: 2,
-    day: 5,
+    day: 5
   },
   {
     name: "Github",
     price: 4,
     autoPay: 1,
     paycheck: 2,
-    day: 14,
+    day: 14
   },
   {
     name: "House",
     price: 970.79,
     autoPay: 1,
     paycheck: 2,
-    day: 11,
+    day: 11
   },
   {
     name: "EnerBank USA",
@@ -30,7 +30,7 @@ const bills = [
     autoPay: 0,
     paycheck: 1,
     url: "https://enerbankpayments.com/#/person/login/",
-    day: 16,
+    day: 16
   },
   {
     name: "Gas",
@@ -38,34 +38,34 @@ const bills = [
     autoPay: 1,
     paycheck: 2,
     url: "https://www.oklahomanaturalgas.com/",
-    day: 11,
+    day: 11
   },
   {
     name: "Ipsy",
     price: 27.23,
     autoPay: 1,
     paycheck: 2,
-    day: 1,
+    day: 1
   },
   {
     name: "Chewy",
     price: 43.39,
     autoPay: 1,
     paycheck: 2,
-    day: 7,
+    day: 7
   },
   {
     name: "VidAngel",
     price: 9.99,
     autoPay: 1,
     paycheck: 2,
-    day: 1,
+    day: 1
   },
   {
     name: "Disney+",
     price: 6.99,
     autoPay: 1,
-    paycheck: 2,
+    paycheck: 2
   },
   {
     name: "Sapulpa Times",
@@ -73,7 +73,7 @@ const bills = [
     autoPay: 1,
     paycheck: 2,
     budget: 1,
-    day: 28,
+    day: 28
   },
   {
     name: "Tulsa World",
@@ -81,50 +81,35 @@ const bills = [
     autoPay: 1,
     paycheck: 1,
     budget: 1,
-    day: 16,
-  },
-  {
-    name: "New York Times",
-    price: 40,
-    autoPay: 1,
-    paycheck: 1,
-    budget: 1,
-    day: 26,
+    day: 16
   },
   {
     name: "JW.org",
     price: 100,
     autoPay: 1,
     paycheck: 1,
-    day: 15,
+    day: 15
   },
   {
     name: "Instacart Express",
     price: 9.99,
     autoPay: 1,
     paycheck: 2,
-    day: 14,
-  },
-  {
-    name: "New England Coffee",
-    price: 33.16,
-    autoPay: 1,
-    paycheck: 1,
-    day: 28,
+    day: 14
   },
   {
     name: "Adobe InDesign",
     price: 20.99,
     autoPay: 1,
     paycheck: 1,
-    day: 11,
+    day: 11
   },
   {
     name: "Water",
     priceEstimate: 60,
     autoPay: 0,
     url: "https://www.municipalonlinepayments.com/bixbyok/utilities",
-    paycheck: 2,
+    paycheck: 2
   },
   {
     name: "Electric",
@@ -132,81 +117,81 @@ const bills = [
     autoPay: 0,
     url: "https://psoklahoma.com/account/bills/",
     paycheck: 2,
-    day: 2,
+    day: 2
   },
   {
     name: "YouTube Premium",
     price: 14.99,
     autoPay: 1,
     paycheck: 2,
-    day: 14,
+    day: 14
   },
   {
     name: "BarkBox",
     autoPay: 1,
     paycheck: 2,
     price: 27.23,
-    day: 3,
+    day: 3
   },
   {
     name: "DigitalOcean",
-    price: 15.7,
+    price: 10.7,
     autoPay: 1,
     paycheck: 2,
-    day: 28,
+    day: 28
   },
   {
     name: "Hulu",
-    price: 11.99,
+    price: 12.99,
     autoPay: 1,
     paycheck: 1,
-    day: 21,
+    day: 21
   },
   {
     name: "Netflix",
     price: 12.99,
     autoPay: 1,
     paycheck: 2,
-    day: 4,
+    day: 4
   },
   {
     name: "State Farm",
     price: 75.41,
     autoPay: 1,
     paycheck: 1,
-    day: 17,
+    day: 17
   },
   {
     name: "Blue Car",
     price: 359.86,
     autoPay: 0,
-    paycheck: 1,
+    paycheck: 1
   },
   {
     name: "Trash",
     price: 21,
     autoPay: 1,
-    paycheck: 1,
+    paycheck: 1
   },
   {
     name: "Vivint Service",
     price: 44.99,
     autoPay: 1,
-    paycheck: 1,
+    paycheck: 1
   },
   {
     name: "Vivint Equipment",
     price: 26.92,
     autoPay: 1,
-    paycheck: 1,
+    paycheck: 1
   },
   {
     name: "Arlo Smart",
-    price: 4.99,
+    price: 9.99,
     autoPay: 1,
     paycheck: 1,
-    day: 14,
-  },
+    day: 14
+  }
 ];
 
 export default bills;
