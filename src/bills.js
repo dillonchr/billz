6ad1181d780bcab0002452c44938ee70e06d7256
Dillon Christensen <j.dillon.christensen@gmail.com>
2021-12-15 09:16:51 -0600

    removed switch and changed colors and layout (#5)

--- src/bills.js
+++ src/bills.js
@@ -4,11 +4,89 @@
 //   2 = 30th of the month paycheck
 const bills = [
   {
+    name: "Adobe InDesign",
+    price: 20.99,
+    autoPay: 1,
+    paycheck: 1,
+    day: 11
+  },
+  {
+    name: "Arlo Smart",
+    price: 9.99,
+    autoPay: 1,
+    paycheck: 1,
+    day: 14
+  },
+  {
+    name: "BarkBox",
+    autoPay: 1,
+    paycheck: 2,
+    price: 27.23,
+    day: 3
+  },
+  {
+    name: "Blue Car",
+    price: 359.86,
+    autoPay: 0,
+    paycheck: 1
+  },
+  {
+    name: "Chewy",
+    price: 43.39,
+    autoPay: 1,
+    paycheck: 2,
+    day: 7
+  },
+  {
     name: "Cox",
     price: 113.33,
     autoPay: 1,
     paycheck: 2,
     day: 5
+  },
+  {
+    name: "DigitalOcean",
+    price: 10.7,
+    autoPay: 1,
+    paycheck: 2,
+    day: 28
+  },
+  {
+    name: "Disney+",
+    price: 6.99,
+    autoPay: 1,
+    paycheck: 2
+  },
+  {
+    name: "Electric Bill",
+    priceEstimate: 100,
+    autoPay: 0,
+    url: "https://psoklahoma.com/account/bills/",
+    paycheck: 2,
+    day: 2
+  },
+  {
+    name: "EnerBank USA",
+    price: 422.04,
+    autoPay: 0,
+    paycheck: 1,
+    url: "https://enerbankpayments.com/#/person/login/",
+    day: 16
+  },
+  {
+    name: "Funimation",
+    price: 7.99,
+    paycheck: 2,
+    day: 11,
+    autoPay: 1
+  },
+  {
+    name: "Gas Bill - ONG",
+    priceEstimate: 50,
+    autoPay: 1,
+    paycheck: 2,
+    url: "https://www.oklahomanaturalgas.com/",
+    day: 11
   },
   {
     name: "Github",
@@ -25,20 +103,18 @@ const bills = [
     day: 11
   },
   {
-    name: "EnerBank USA",
-    price: 422.04,
-    autoPay: 0,
+    name: "Hulu",
+    price: 12.99,
+    autoPay: 1,
     paycheck: 1,
-    url: "https://enerbankpayments.com/#/person/login/",
-    day: 16
+    day: 21
   },
   {
-    name: "Gas",
-    priceEstimate: 50,
+    name: "Instacart Express",
+    price: 9.99,
     autoPay: 1,
     paycheck: 2,
-    url: "https://www.oklahomanaturalgas.com/",
-    day: 11
+    day: 14
   },
   {
     name: "Ipsy",
@@ -48,24 +124,26 @@ const bills = [
     day: 1
   },
   {
-    name: "Chewy",
-    price: 43.39,
+    name: "Netflix",
+    price: 12.99,
     autoPay: 1,
     paycheck: 2,
-    day: 7
+    day: 4
   },
   {
-    name: "VidAngel",
+    name: "Packt Pub Sub",
     price: 9.99,
     autoPay: 1,
-    paycheck: 2,
-    day: 1
+    paycheck: 1,
+    budget: 1,
+    day: 19
   },
   {
-    name: "Disney+",
-    price: 6.99,
+    name: "JW.org",
+    price: 100,
     autoPay: 1,
-    paycheck: 2
+    paycheck: 1,
+    day: 15
   },
   {
     name: "Sapulpa Times",
@@ -76,6 +154,19 @@ const bills = [
     day: 28
   },
   {
+    name: "State Farm",
+    price: 75.41,
+    autoPay: 1,
+    paycheck: 1,
+    day: 17
+  },
+  {
+    name: "Trash",
+    price: 21,
+    autoPay: 1,
+    paycheck: 1
+  },
+  {
     name: "Tulsa World",
     price: 10.99,
     autoPay: 1,
@@ -84,92 +175,15 @@ const bills = [
     day: 16
   },
   {
-    name: "JW.org",
-    price: 100,
-    autoPay: 1,
-    paycheck: 1,
-    day: 15
-  },
-  {
-    name: "Instacart Express",
+    name: "VidAngel",
     price: 9.99,
     autoPay: 1,
     paycheck: 2,
-    day: 14
+    day: 1
   },
   {
-    name: "Adobe InDesign",
-    price: 20.99,
-    autoPay: 1,
-    paycheck: 1,
-    day: 11
-  },
-  {
-    name: "Water",
-    priceEstimate: 60,
-    autoPay: 0,
-    url: "https://www.municipalonlinepayments.com/bixbyok/utilities",
-    paycheck: 2
-  },
-  {
-    name: "Electric",
-    priceEstimate: 100,
-    autoPay: 0,
-    url: "https://psoklahoma.com/account/bills/",
-    paycheck: 2,
-    day: 2
-  },
-  {
-    name: "YouTube Premium",
-    price: 14.99,
-    autoPay: 1,
-    paycheck: 2,
-    day: 14
-  },
-  {
-    name: "BarkBox",
-    autoPay: 1,
-    paycheck: 2,
-    price: 27.23,
-    day: 3
-  },
-  {
-    name: "DigitalOcean",
-    price: 10.7,
-    autoPay: 1,
-    paycheck: 2,
-    day: 28
-  },
-  {
-    name: "Hulu",
-    price: 12.99,
-    autoPay: 1,
-    paycheck: 1,
-    day: 21
-  },
-  {
-    name: "Netflix",
-    price: 12.99,
-    autoPay: 1,
-    paycheck: 2,
-    day: 4
-  },
-  {
-    name: "State Farm",
-    price: 75.41,
-    autoPay: 1,
-    paycheck: 1,
-    day: 17
-  },
-  {
-    name: "Blue Car",
-    price: 359.86,
-    autoPay: 0,
-    paycheck: 1
-  },
-  {
-    name: "Trash",
-    price: 21,
+    name: "Vivint Equipment",
+    price: 26.92,
     autoPay: 1,
     paycheck: 1
   },
@@ -180,16 +194,17 @@ const bills = [
     paycheck: 1
   },
   {
-    name: "Vivint Equipment",
-    price: 26.92,
-    autoPay: 1,
-    paycheck: 1
+    name: "Water Bill",
+    priceEstimate: 60,
+    autoPay: 0,
+    url: "https://www.municipalonlinepayments.com/bixbyok/utilities",
+    paycheck: 2
   },
   {
-    name: "Arlo Smart",
-    price: 9.99,
+    name: "YouTube Premium",
+    price: 14.99,
     autoPay: 1,
-    paycheck: 1,
+    paycheck: 2,
     day: 14
   }
 ];
